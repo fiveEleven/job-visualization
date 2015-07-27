3af7a2b8469704396913ea6630547dabefe42df5
Cindy Jo <sungaej@gmail.com>
2015-07-27 16:14:50 -0700

    Created a script to add a data to database

---
+++ jobvisualization/apps/data_storage/views.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from apps.data_storage import job_query as City
+from apps.data_storage.models import WriteOnly
+
+
+WriteOnly.objects.all().delete()
+
+def updateBase():
+	print "updateBase"
+	WriteOnly.objects.all().delete()
+	results = City.job_query("Front End")
+	for result in results:
+		WriteOnly.objects.create(
+			num_jobs = result['numJobs'],
+			city_name = result['name'],
+			state = result['stateName'],
+			latitude = result['latitude'],
+			longitude = result['longitude'],
+			job_title = "Front End",
+			created_at = datetime.now()
+		)
+
+	results = City.job_query("Back End")
+	for result in results:
+		WriteOnly.objects.create(
+			num_jobs = result['numJobs'],
+			city_name = result['name'],
+			state = result['stateName'],
+			latitude = result['latitude'],
+			longitude = result['longitude'],
+			job_title = "Back End",
+			created_at = datetime.now()
+		)
+
+	results = City.job_query("Full Stack")
+	for result in results:
+		WriteOnly.objects.create(
+			num_jobs = result['numJobs'],
+			city_name = result['name'],
+			state = result['stateName'],
+			latitude = result['latitude'],
+			longitude = result['longitude'],
+			job_title = "Full Stack",
+			created_at = datetime.now()
+		)
+	return True
+
+updateBase()
+
+
