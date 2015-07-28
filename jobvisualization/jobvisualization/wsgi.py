d4ae8dc47c83cce6fd3d76e54465bcedd22e747b
Nick McCrory <n.mccrory@icloud.com>
2015-07-28 09:50:22 -0700

    adjusted wsgi.py file back to normal state

--- jobvisualization/jobvisualization/wsgi.py
+++ jobvisualization/jobvisualization/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobvisualization.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
