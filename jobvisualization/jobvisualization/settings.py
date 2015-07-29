05060ea269c397ac1c903babae5fad1b28d82eb6
Nick McCrory <n.mccrory@icloud.com>
2015-07-28 22:16:45 -0700

    Changed some files for heroku

--- jobvisualization/jobvisualization/settings.py
+++ jobvisualization/jobvisualization/settings.py
@@ -101,4 +101,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
