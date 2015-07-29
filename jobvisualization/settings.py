b2c26fc8156b1c524da841eecd3835f41aaa18b7
Morgan Brooke <morganbwright@gmail.com>
2015-07-29 13:24:09 -0700

    updated settings.py

--- jobvisualization/settings.py
+++ jobvisualization/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
+# import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
