699118c36f49dce761724ef15c3e1e15d39b1197
Stephen <e2stephen@cox.net>
2015-07-29 19:23:22 -0700

    added __init__.py and urls.py to jobvisualization subdirectory. Added .ds_store to .gitignore

--- jobvisualization/settings.py
+++ jobvisualization/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-# import dj_database_url
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
